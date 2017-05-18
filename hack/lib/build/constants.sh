68c74ff4aede994c6b76982c84d64536f65814bc
Jeff Peeler <jpeeler@redhat.com>
2017-05-18 10:32:20 -0400

    origin build: modify hard coded path
    Also change working dir to be based off constant rather than poking inside
    the build image to determine.

--- hack/lib/build/constants.sh
+++ hack/lib/build/constants.sh
@@ -11,7 +11,7 @@ readonly OS_LOCAL_RELEASEPATH="${OS_OUTPUT}/releases"
 readonly OS_OUTPUT_BINPATH="${OS_OUTPUT}/bin"
 readonly OS_OUTPUT_PKGDIR="${OS_OUTPUT}/pkgdir"
 
-readonly OS_GO_PACKAGE=github.com/openshift/origin
+readonly OS_GO_PACKAGE=github.com/kubernetes-incubator/service-catalog
 
 readonly OS_SDN_COMPILE_TARGETS_LINUX=(
   pkg/sdn/plugin/sdn-cni-plugin
