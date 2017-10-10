3aacfedec6f8d3d07a5b922e0fcd4a9b28a0e5d2
Jay Boyd <jaboyd@redhat.com>
2017-10-10 11:28:39 -0400

    carry: Set external plan name for service-catalog walkthrough

--- test/e2e/walkthrough.go
+++ test/e2e/walkthrough.go
@@ -218,6 +218,7 @@ var _ = framework.ServiceCatalogDescribe("walkthrough", func() {
 			Spec: v1beta1.ServiceInstanceSpec{
 				PlanReference: v1beta1.PlanReference{
 					ExternalClusterServiceClassName: serviceclassName,
+					ExternalClusterServicePlanName:  "default",
 				},
 			},
 		}
