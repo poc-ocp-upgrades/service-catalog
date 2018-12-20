bfe991ef9dd65b8dff983edabfc11dd41ed265cc
Jay Boyd <jaboyd@redhat.com>
2018-12-20 13:57:47 -0500

    refine preparing for and running e2e.  Save off key artifacts

--- hack/create-subscription.sh
+++ hack/create-subscription.sh
@@ -2,8 +2,8 @@
 set -o errexit
 set -o nounset
 set -o pipefail
-set -o xtrace
 
+echo Preparing Service Catalog E2E at $(TZ=EST date) / $(TZ=UTC date +"%k:%M:%S %Z")
 oc create namespace kube-service-catalog || true
 oc label ns kube-service-catalog ns=kube-service-catalog || true
 
