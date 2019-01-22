10b966f2cc5c9532b4470f33e3249df5d411030d
Jay Boyd <jaboyd@redhat.com>
2019-01-22 15:50:59 -0500

    reworked installing Catalog via Marketplace

--- hack/create-subscription.sh
+++ hack/create-subscription.sh
@@ -4,29 +4,36 @@ set -o nounset
 set -o pipefail
 
 echo Preparing Service Catalog E2E at $(TZ=EST date) / $(TZ=UTC date +"%k:%M:%S %Z")
-oc create namespace kube-service-catalog || true
-oc label ns kube-service-catalog ns=kube-service-catalog || true
 
-cat <<'EOF' | oc create -f -
-apiVersion: operators.coreos.com/v1alpha1
-kind: Subscription
-metadata:
-  name: svcat
-  namespace: kube-service-catalog
-spec:
-  channel: alpha
-  name: svcat
-  source: rh-operators
-  installPlanApproval: Automatic
-  catalogSourceNamespace: kube-service-catalog
----
-apiVersion: operators.coreos.com/v1alpha2
-kind: OperatorGroup
-metadata:
-  name: service-catalog
-  namespace: kube-service-catalog
-spec:
-  selector:
-    matchLabels:
-      ns: kube-service-catalog
-EOF
+
+oc create namespace kube-service-catalog || true
+
+oc apply -f hack/svcat-catalogsourceconfig.yaml
+
+# wait for the catalog source pod
+#oc get pods -lolm.catalogSource=service-catalog -n kube-service-catalog
+
+
+echo "`date +%T` Waiting for for catalogsource to be ready..."
+TARGET="$(date -d '5 minutes' +%s)"
+NOW="$(date +%s)"
+while [[ "${NOW}" -lt "${TARGET}" ]]; do
+  REMAINING="$((TARGET - NOW))"
+  oc get pods -lolm.catalogSource=service-catalog -n kube-service-catalog || true
+  JSONPATH="{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status};{end}{end}"
+  STATUS=`oc get pods -lolm.catalogSource=service-catalog -n kube-service-catalog -o jsonpath="$JSONPATH"`
+  if [[ $STATUS == *"Ready=True"* ]];then
+    break
+  fi
+  sleep 20
+  NOW="$(date +%s)"
+done
+
+if [ "${NOW}" -ge "${TARGET}" ];then
+    echo "`date +%T`: warning: catalogsource not ready"
+else
+    echo "`date +%T`: catalogsource is running & ready, proceeding"
+fi
+
+oc apply -f hack/svcat-operatorgroup.yaml
+oc apply -f hack/svcat-subscription.yaml
