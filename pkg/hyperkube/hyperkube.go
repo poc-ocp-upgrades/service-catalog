cf2f161c3107ae9af0b365957db853b4c52985f1
Jay Boyd <jaboyd@redhat.com>
2019-03-20 12:59:48 -0400

    revert logging back to reporting pkg.VERSION
    Format: Service Catalog version v4.0.0-0.1.42+d79ee7d-dirty;Upstream:v0.1.42 (built 2019-03-20T16:52:23Z)

--- pkg/hyperkube/hyperkube.go
+++ pkg/hyperkube/hyperkube.go
@@ -189,7 +189,7 @@ RunAgain:
 
 	logs.InitLogs()
 	defer logs.FlushLogs()
-	klog.Infof("Service Catalog version %s (built %s)", version.Get().String(), version.Get().BuildDate)
+	klog.Infof("Service Catalog version %s (built %s)", pkg.VERSION, version.Get().BuildDate)
 	if !s.RespectsStopCh {
 		// For commands that do not respect the stopCh, we run them in a go
 		// routine and leave them running when stopCh is closed.
